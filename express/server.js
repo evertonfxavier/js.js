f833e60b257ca1e90577ec1a93a139edb3afb413
Everton Xavier <evertonfxavier@gmail.com>
2022-11-14 18:12:21 -0300

    req params, req query, req body

--- express/server.js
+++ express/server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
 
-//o express vai ajudar a trabalhar com as rotas (get, post, put, delete, etc)
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (request, response) => {
+  // response.send("hello world");
+  response.send(`
+    <form action="/" method="POST">
+        Nome do cliente: <input type="text" name="nome" />
+        <button>vai</button>
+    </form>
+  `);
+});
+
+app.get("/teste/:idUser", (request, response) => {
+  console.log(request.params);
   response.send("hello world");
 });
 
-app.get("/contact", (request, response) => {
-  response.send("salve meu contato");
+app.post("/", (request, response) => {
+  console.log(request.body);
+  response.send("recebido");
 });
 
 app.listen(3333, () => console.log("servidor execudando na porta 3333"));
