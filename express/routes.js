ac0facdeaa1edf2f76ec695c26e5a77ca416ccb4
Everton Xavier <evertonfxavier@gmail.com>
2022-11-14 18:47:12 -0300

    express views

--- express/routes.js
+++ express/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route = express.Router();
 
-const homeController = require("./controllers/homeController");
+const homeController = require("./src/controllers/homeController");
 
 //rotas da home
 route.get("/", homeController.paginaInicial);
--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -8,6 +9,9 @@ app.use(
     extended: true,
   })
 );
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
 app.use(routes);
 
