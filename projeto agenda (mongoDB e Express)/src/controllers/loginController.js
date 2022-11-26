e44e91bcd0033489005d967107191e69c212c91b
Everton Xavier <evertonfxavier@gmail.com>
2022-11-26 14:34:41 -0300

    criando usuários 1

--- projeto agenda (mongoDB e Express)/src/controllers/loginController.js
+++ projeto agenda (mongoDB e Express)/src/controllers/loginController.js
@@ -1,3 +1,30 @@
+const Login = require("../models/LoginModel");
+
 exports.index = (req, res) => {
-  res.render('login');
+  res.render("login");
+};
+
+exports.register = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        console.log('deu erro');
+        return res.redirect('/login/index');
+      });
+      return;
+    }
+    
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+      console.log('deu certo');
+      return res.redirect('/login/index');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
 };
