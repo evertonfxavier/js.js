3af21e9fd2724e30054064c6085d11644dc91a35
Everton Xavier <evertonfxavier@gmail.com>
2022-11-27 15:29:32 -0300

    cadastrando contatos

--- projeto agenda (mongoDB e Express)/routes.js
+++ projeto agenda (mongoDB e Express)/routes.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
 
-const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
+const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
+
+const { loginRequired } = require("./src/middlewares/middlewares");
 
 // Rotas da home
-route.get('/', homeController.index);
+route.get("/", homeController.index);
 
 // Rotas de login
-route.get('/login/index', loginController.index);
-route.post('/login/register', loginController.register);
-route.post('/login/login', loginController.login);
-route.get('/login/logout', loginController.logout);
+route.get("/login/index", loginController.index);
+route.post("/login/register", loginController.register);
+route.post("/login/login", loginController.login);
+route.get("/login/logout", loginController.logout);
 
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
 
 module.exports = route;
--- projeto agenda (mongoDB e Express)/src/controllers/contatoController.js
+++ projeto agenda (mongoDB e Express)/src/controllers/contatoController.js
@@ -1,3 +1,44 @@
-exports.paginaInicial = (req, res) => {
-  res.send('Obrigado por entrar em contato.');
+const Contato = require("../models/ContatoModel");
+
+exports.index = (req, res) => {
+  res.render("contato", {
+    contato: {},
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("/contato/index"));
+      return;
+    }
+
+    req.flash("success", "Contato registrado com sucesso.");
+    // req.session.save(() => res.redirect('/contato/index'));
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    );
+    return;
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
+};
+
+exports.editIndex = async function (req, res) {
+  // console.log('temmm0');
+  if (!req.params.id) return res.render("404");
+  // console.log('temmm1');
+
+  const contato = await Contato.buscaPorId(req.params.id);
+  // console.log('temmm2', contato);
+  if (!contato) return res.render("404");
+
+  // console.log('temm3', contato);
+
+  res.render("contato", { contato });
 };
